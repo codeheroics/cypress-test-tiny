9e494dbeaf1ba4edbb2b1a218c649952f322aaa4
Hugo Agbonon <hugo.agbonon-ext@dashlane.com>
2020-04-07 18:20:45 +0200

    demo of spaces issue

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("page", () => {
+  it("does not work with the original string", () => {
+    cy.visit("https://codeheroics.github.io/nbsp/");
+    cy.contains("I am text containing nonbreaking spaces which are important in some languages for punctuation");
+  });
+  it("works with all spaces replaced with normal spaces inside the test", () => {
+    cy.visit("https://codeheroics.github.io/nbsp/");
+    cy.contains("I am text containing nonbreaking spaces which are important in some languages for punctuation");
+  });
+});
